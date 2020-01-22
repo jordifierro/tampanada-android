6e6ee48b4a6555c307741c455dec22adb41565cc
Jordi Fierro <jordifierromulero@gmail.com>
2020-01-22 21:33:28 +0100

    Make random streaming url sound using MediaPlayer

--- app/src/main/java/com/tampanada/radio/MainActivity.kt
+++ app/src/main/java/com/tampanada/radio/MainActivity.kt
@@ -1,29 +1,44 @@
 package com.tampanada.radio
 
+import android.media.AudioManager
+import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Window
-import android.webkit.WebView
-import android.webkit.WebViewClient
+import android.media.AudioAttributes
+import android.os.Build
+import android.util.Log
+
 
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var webView: WebView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main)
 
-        webView = findViewById(R.id.webview)
-        webView.webViewClient = object : WebViewClient() {
-            override fun shouldOverrideUrlLoading(view: WebView?, url: String?): Boolean {
-                view?.loadUrl(url)
-                return true
-            }
+        val url = "http://vprbbc.streamguys.net/vprbbc24.mp3"
+        val player: MediaPlayer? = MediaPlayer()
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            player!!.setAudioAttributes(
+                AudioAttributes.Builder()
+                    .setUsage(AudioAttributes.USAGE_MEDIA)
+                    .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
+                    .setLegacyStreamType(AudioManager.STREAM_MUSIC)
+                    .build()
+            )
+        } else {
+            player!!.setAudioStreamType(AudioManager.STREAM_MUSIC)
         }
-        webView.settings.javaScriptEnabled = true
-        webView.settings.javaScriptCanOpenWindowsAutomatically = true
-        webView.loadUrl("http://tampanadaradio.com")
+
+        player.setOnPreparedListener {
+            it.start()
+        }
+        player.setOnErrorListener( MediaPlayer.OnErrorListener { mediaPlayer, i, i2 ->
+            Log.e("tampa", "error " + i + i2)
+            false
+        })
+        player.setDataSource(url)
+        player.prepareAsync()
     }
 }
