fea668ee77fcf62d3fb641f1e963ee8c01f5139e
Jordi Fierro <jordifierromulero@gmail.com>
2020-05-09 18:03:00 +0200

    Fix notification issue for version > 28

--- app/src/main/java/com/tampanada/radio/ForegroundService.kt
+++ app/src/main/java/com/tampanada/radio/ForegroundService.kt
@@ -1,12 +1,14 @@
 package com.tampanada.radio
 
-import android.app.Notification
-import android.app.PendingIntent
-import android.app.Service
+import android.app.*
+import android.content.Context
 import android.content.Intent
 import android.graphics.BitmapFactory
+import android.graphics.Color
 import android.net.Uri
+import android.os.Build
 import android.os.IBinder
+import androidx.annotation.RequiresApi
 import androidx.core.app.NotificationCompat
 import com.google.android.exoplayer2.C
 import com.google.android.exoplayer2.SimpleExoPlayer
@@ -79,9 +81,13 @@ class ForegroundService : Service() {
     }
 
     private fun createNotification(action: String): Notification {
+        val channelId =
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
+                createNotificationChannel("com.tampanada.radio", "Tampanada Radio")
+            else ""
         val intent = Intent()
         val pendingIntent = PendingIntent.getActivity(this, 0, intent, 0)
-        val builder: NotificationCompat.Builder = NotificationCompat.Builder(this)
+        val builder: NotificationCompat.Builder = NotificationCompat.Builder(this, channelId)
 
         val bigTextStyle: NotificationCompat.BigTextStyle = NotificationCompat.BigTextStyle()
         bigTextStyle.setBigContentTitle("Tampanada Radio")
@@ -159,5 +165,17 @@ class ForegroundService : Service() {
         broadCastIntent.action = MainActivity.PlayerStateBroadCastReceiver.BROADCAST_ACTION
         broadCastIntent.putExtra(MainActivity.PlayerStateBroadCastReceiver.STATE, state);
         sendBroadcast(broadCastIntent)
+    }
+
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun createNotificationChannel(channelId: String, channelName: String): String{
+        val chan = NotificationChannel(channelId,
+            channelName, NotificationManager.IMPORTANCE_NONE)
+        chan.lightColor = Color.BLUE
+        chan.lockscreenVisibility = Notification.VISIBILITY_PRIVATE
+        val service = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        service.createNotificationChannel(chan)
+        return channelId
     }
 }
